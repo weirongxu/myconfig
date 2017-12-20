3b6ea211bbe94d4b6a8890570fe6198c059d7cef
Weirong Xu <weirongxu.raidou@gmail.com>
2017-12-20 17:47:20 +0800

    nvim as default editor

--- myscripts/variables/vim.sh
+++ myscripts/variables/vim.sh
@@ -1,8 +1,8 @@
 export NVIM_LISTEN_ADDRESS="/tmp/neovim/neovim"
-if exists_cmd 'vim' ; then
-  export VISUAL=vim
-  export EDITOR=vim
-elif exists_cmd 'nvim' ; then
+if exists_cmd 'nvim' ; then
   export VISUAL=nvim
   export EDITOR=nvim
+elif exists_cmd 'vim' ; then
+  export VISUAL=vim
+  export EDITOR=vim
 fi
