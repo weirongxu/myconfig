f68d58b3d46ce910f1b03e3f6c68a7126d013b0a
Weirong Xu <weirongxu.raidou@gmail.com>
2018-03-20 11:17:06 +0800

    fix pyenv

--- myscripts/environment/python.sh
+++ myscripts/environment/python.sh
@@ -3,7 +3,7 @@ try-source "/usr/local/bin/virtualenvwrapper.sh"
 export WORKON_HOME="$HOME/.virtualenv"
 export PYTHONPATH="$HOME/Program/pydebug/pythonlib/"
 if exists-cmd brew; then
-  CFLAGS="-I$(brew --prefix openssl)/include"
-  LDFLAGS="-L$(brew --prefix openssl)/lib"
+  export CFLAGS="-I$(brew --prefix openssl)/include"
+  export LDFLAGS="-L$(brew --prefix openssl)/lib"
 fi
 add-path $(pyenv root)/shims
