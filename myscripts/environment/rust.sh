8ead5c1151d0b8c96b65faf9250bcbfdc0621baf
Weirong Xu <weirongxu.raidou@gmail.com>
2018-06-06 09:27:49 +0800

    update environment

--- myscripts/environment/rust.sh
+++ myscripts/environment/rust.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
-add-path "$HOME/.cargo/bin"
-# try-source "$HOME/.cargo/env"
+local cargo_env_path=$HOME/.cargo/env
+if [[ -f $cargo_env_path ]]; then
+  try-source "$HOME/.cargo/env"
+else
+  add-path "$HOME/.cargo/bin"
+fi
+
+local src_paths=(
+  /usr/local/share/rust/rust_src
+)
+if exists-cmd rustc; then
+  src_paths+=("$(rustc --print sysroot)/lib/rustlib/src/rust/src")
+fi
+for _path in ${src_paths[@]}; do
+  if [[ -d $_path ]]; then
+    export RUST_SRC_PATH=$_path
+    break
+  fi
+done
