6399230198e8db805d113a61d23b85ed359b24ab
Weirong Xu <weirongxu.raidou@gmail.com>
2018-06-25 10:37:46 +0800

    update fish

--- home/.config/fish/conf.d/my.fish
+++ home/.config/fish/conf.d/my.fish
@@ -6,4 +6,8 @@ function fish_user_key_bindings
     bind -M $mode \ca beginning-of-line
     bind -M $mode \ce end-of-line
   end
+
+  if type --quiet "fzf_key_bindings"
+    fzf_key_bindings
+  end
 end
