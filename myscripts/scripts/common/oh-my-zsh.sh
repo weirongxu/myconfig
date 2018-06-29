4b642ffe1a81bf72595a645f0d79d75958791e95
Weirong Xu <weirongxu.raidou@gmail.com>
2018-06-29 11:05:42 +0800

    fix oh-my-zsh

--- myscripts/scripts/common/oh-my-zsh.sh
+++ myscripts/scripts/common/oh-my-zsh.sh
@@ -1,6 +1,8 @@
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 # sed -i -e 's#source \$ZSH/oh-my-zsh\.sh##g' ~/.zshrc
