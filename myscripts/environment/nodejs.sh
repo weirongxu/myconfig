d0d7d4e8daa91249176fe87311b623d9a735a094
Weirong Xu <weirongxu.raidou@gmail.com>
2018-12-17 11:42:12 +0800

    fix nvim

--- myscripts/environment/nodejs.sh
+++ myscripts/environment/nodejs.sh
@@ -1,8 +1,8 @@
 export NVM_DIR="$HOME/.nvm"
-# if [[ ! is-fish ]]; then
-#   try-source "$NVM_DIR/nvm.sh"
-#   try-source "$NVM_DIR/bash_completion"
-# fi
+if ! is-fish; then
+  try-source "$NVM_DIR/nvm.sh"
+  try-source "$NVM_DIR/bash_completion"
+fi
 if exists-cmd npm; then
   add-path $(npm -g bin)
 fi
