f7a611af58a5e53caf4763094dfc665aef00d078
Weirong Xu <weirongxu.raidou@gmail.com>
2020-01-15 00:02:54 +0800

    fix starship wrap in fish

--- myscripts/fish/starship.fish
+++ myscripts/fish/starship.fish
@@ -4,15 +4,15 @@ if type -q "starship"
   functions --copy fish_prompt fish_original_prompt
 
   function truncate_line
-    set max_length (math $COLUMNS - 5)
+    set max_length $argv[1]
     while read -l line
       set length (string replace -a -r "\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]" '' $line | string length)
       if [ $length -lt $max_length ]
-        echo -e $line
+        echo $line
       else
         set sep (string match -r '\\b(?:is|via)\\b' $line)
         if [ -n "$sep" ]
-          string replace -r '\\b(?:is|via)\\b' \n$sep $line
+          string replace -r '\\b(?:is|via)\\b' \n$sep $line | truncate_line $max_length
         else
           echo $line | fmt -w $max_length
         end
@@ -21,6 +21,6 @@ if type -q "starship"
   end
 
   function fish_prompt
-    fish_original_prompt | truncate_line
+    fish_original_prompt | truncate_line (math $COLUMNS)
   end
 end
