76fea47c5dc0202942d987714b0bd7b0a27c43ee
Weirong Xu <weirongxu.raidou@gmail.com>
2021-06-10 10:13:29 +0800

    fix: pyenv init

--- myscripts/environment/python.sh
+++ myscripts/environment/python.sh
@@ -7,5 +7,8 @@ if exists-cmd brew; then
   export LDFLAGS="-L$(brew --prefix openssl)/lib"
 fi
 if exists-cmd pyenv; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
 fi
